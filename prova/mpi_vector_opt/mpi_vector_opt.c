de454f9fa2e25fbf0c15c57ba5365b079932bb25
AlefMemTav <garcia.lucas@aluno.unb.br>
2025-01-13 11:59:33 -0300

    refatora

--- prova/mpi_vector_opt/mpi_vector_opt.c
+++ prova/mpi_vector_opt/mpi_vector_opt.c
@@ -1,70 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#define MASTER 0
 
-int main(int argc, char** argv) {
-    int rank, size;
-    int *vector = NULL;
-    int *local_vector = NULL;
-    int vector_size = 1000000; // Tamanho total do vetor
-    
+int main(int argc, char* argv[]) {
+    int rank, nprocs;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     
-    // Calcular o tamanho local para cada processo
-    int local_size = vector_size / size;
-    int remainder = vector_size % size;
-    
-    // Ajustar tamanho local se houver resto
-    if (rank < remainder) {
-        local_size++;
+    if (rank == MASTER) {
+        printf("Tamanho do vetor: ");
+        fflush(stdout);
     }
     
-    // Alocar apenas a parte local necessária
-    local_vector = (int*)malloc(local_size * sizeof(int));
+    int tamvet;
+    MPI_Bcast(&tamvet, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
+    
+    // Calcula o tamanho do chunk local para cada processo
+    int chunk = tamvet/nprocs;
+    int resto = tamvet % nprocs;
+    int local_size = chunk + (rank < resto ? 1 : 0);
+    
+    // Aloca apenas a memória necessária para o chunk local
+    int *local_v = (int *) malloc(local_size * sizeof(int));
     
     // Apenas o MASTER aloca e inicializa o vetor completo
-    if (rank == 0) {
-        vector = (int*)malloc(vector_size * sizeof(int));
-        // Inicialização do vetor
-        for (int i = 0; i < vector_size; i++) {
-            vector[i] = i;
+    int *v = NULL;
+    if (rank == MASTER) {
+        v = (int *) malloc(tamvet * sizeof(int));
+        for (int i=0; i < tamvet; i++) {
+            v[i] = i*2;  // Mantendo a mesma inicialização do original
         }
     }
     
-    // Calcular os deslocamentos para cada processo
-    int *sendcounts = (int*)malloc(size * sizeof(int));
-    int *displs = (int*)malloc(size * sizeof(int));
+    // Calcula os deslocamentos e tamanhos para cada processo
+    int *sendcounts = (int *) malloc(nprocs * sizeof(int));
+    int *displs = (int *) malloc(nprocs * sizeof(int));
     int disp = 0;
     
-    for (int i = 0; i < size; i++) {
-        sendcounts[i] = vector_size / size;
-        if (i < remainder) {
-            sendcounts[i]++;
-        }
+    for (int i = 0; i < nprocs; i++) {
+        sendcounts[i] = chunk;
+        if (i < resto) sendcounts[i]++;
         displs[i] = disp;
         disp += sendcounts[i];
     }
     
-    // Distribuir os dados usando MPI_Scatterv
-    MPI_Scatterv(vector, sendcounts, displs, MPI_INT,
-                 local_vector, local_size, MPI_INT,
-                 0, MPI_COMM_WORLD);
+    // Distribui os dados usando MPI_Scatterv
+    MPI_Scatterv(v, sendcounts, displs, MPI_INT,
+                 local_v, local_size, MPI_INT,
+                 MASTER, MPI_COMM_WORLD);
     
-    // Imprimir elementos locais
-    printf("Processo %d: Primeiros 3 elementos: ", rank);
-    for (int i = 0; i < (local_size < 3 ? local_size : 3); i++) {
-        printf("%d ", local_vector[i]);
+    // Imprime os elementos locais
+    printf("%d/%d: ", rank, nprocs);
+    for (int i=0; i < local_size; i++) {
+        printf("%d ", local_v[i]);
     }
     printf("\n");
     
-    // Liberar memória
-    free(local_vector);
+    // Libera a memória
+    free(local_v);
     free(sendcounts);
     free(displs);
-    if (rank == 0) {
-        free(vector);
+    if (rank == MASTER) {
+        free(v);
     }
     
     MPI_Finalize();
